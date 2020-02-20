ce96cbbad58bbf9072a782f2104df465affc9b21
Michal <rogozmichal@wp.pl>
2020-02-20 18:09:07 +0100

    Master second commit

--- src/main/java/ourGit/First.java
+++ src/main/java/ourGit/First.java
@@ -1,7 +1,13 @@
 package ourGit;
 
 public class First {
+
+    String y;
+
     public static void main(String[] args) {
+
         System.out.println("Mama");
+
+        System.out.println("Dziecko");
     }
 }
