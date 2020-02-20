ac280e1df3b7999472c25804e52084c4282c90ff
Michal <rogozmichal@wp.pl>
2020-02-20 18:07:30 +0100

    Second commit

--- src/main/java/ourGit/First.java
+++ src/main/java/ourGit/First.java
@@ -1,7 +1,11 @@
 package ourGit;
 
 public class First {
+    String x;
     public static void main(String[] args) {
+
         System.out.println("Mama");
+        System.out.println("tata");
+
     }
 }
