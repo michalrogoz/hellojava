25451bd8952bbd25f1abef66e2f9b9d1a2f126ba
zuku0404 <mzuk0493@gmail.com>
2020-02-26 11:23:42 +0100

    new message

--- src/main/java/ourGit/First.java
+++ src/main/java/ourGit/First.java
@@ -1,7 +1,8 @@
 package ourGit;
 
-public class First {
+public class  First {
     public static void main(String[] args) {
         System.out.println("Mama");
+        System.out.println("new message");
     }
 }
