c3dcc66825920f3ac06085d37b3932f3663bf062
Michal <rogozmichal@wp.pl>
2020-03-06 18:59:58 +0100

    Initial commit

---
+++ project/src/main/java/Main.java
@@ -0,0 +1,3 @@
+public class Main {
+// wczytac z pliku jsona do klasy z typami prostymi a nastepnie ta klase zamienic na klase z enumami ( string-> country)
+}
---
+++ project/src/main/java/Row.java
@@ -0,0 +1,9 @@
+public class Row {
+   private String country;
+   private String credit_card;
+   private String money; // mozna sprobowac zrobic to z regexa lub replace
+   private String category;
+}
+
+//uzywac dostpow prywatnych - gettery i settery
+// uzwyac pakietow do lepszej czytelnosci
